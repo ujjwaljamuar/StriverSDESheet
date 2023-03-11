06971e17d727488adcc5a60b7915375420e4f55a
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-03-11 13:57:34 +0530

    *

--- Recursion/Q3_SubsetSum.java
+++ Recursion/Q3_SubsetSum.java
@@ -3,38 +3,35 @@ package Recursion;
 import java.util.*;
 
 public class Q3_SubsetSum {
-    
-    static void findSum(int index, int sum, ArrayList<Integer> arr, int N, ArrayList<Integer> subsetSums){
+
+    static void findSum(int index, int sum, int arr[], int N, ArrayList<Integer> subsetSums) {
 
         // if (pointer reached the last index)
-        if(index == N){
+        if (index == N) {
             subsetSums.add(sum);
             return;
         }
 
         // if the element gets picked
-        findSum(index+1, sum+arr.get(index), arr, N, subsetSums);
+        findSum(index + 1, sum + arr[index], arr, N, subsetSums);
 
         // if the element did not get picked
-        findSum(index+1, sum, arr, N, subsetSums);
+        findSum(index + 1, sum, arr, N, subsetSums);
     }
 
-    static ArrayList<Integer> subsetSumPrb(ArrayList<Integer> arr, int N){
+    static ArrayList<Integer> subsetSumPrb(int arr[], int N) {
         ArrayList<Integer> subsetSums = new ArrayList<>();
 
-        findSum(0,0,arr,N, subsetSums);
+        findSum(0, 0, arr, N, subsetSums);
         Collections.sort(subsetSums);
 
         return subsetSums;
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> input = new ArrayList<>();
-        input.add(3);
-        input.add(1);
-        input.add(2);
+        int input[] = { 3, 1, 2 };
 
-        ArrayList<Integer> result = subsetSumPrb(input, input.size());
+        ArrayList<Integer> result = subsetSumPrb(input, input.length);
 
         System.out.println(result);
     }
