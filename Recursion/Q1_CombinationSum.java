41ad30ef6333f05372f084fa8e94fa6dd04d39db
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-03-12 20:30:25 +0530

    '*'

--- Recursion/Q1_CombinationSum.java
+++ Recursion/Q1_CombinationSum.java
@@ -1,36 +1,43 @@
-package Recursion;
+//package Recursion;
 
 import java.util.*;
 
 public class Q1_CombinationSum {
 
-    static void findCombination(int index, int arr[], int target, List<List<Integer>> ans, List<Integer> comb) {
-        if (index == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList<>(comb));
+    static void findCombination(int index, int[] arr, int target, List<Integer> ds, List<List<Integer>> result){
+        if(index == arr.length){
+            if(target == 0){
+                result.add(new ArrayList<>(ds));
             }
             return;
         }
 
-        if (arr[index] <= target) {
-            comb.add(arr[index]);
-            findCombination(index, arr, target - arr[index], ans, comb);
-            comb.remove(comb.size() - 1);
+        // picked
+        if(arr[index] <= target){
+            ds.add(arr[index]);
+            findCombination(index, arr, target-arr[index], ds, result);
+            ds.remove(ds.size() - 1);
         }
-        findCombination(index + 1, arr, target, ans, comb);
+
+        // not picked
+        findCombination(index+1, arr, target, ds, result);
     }
 
-    static List<List<Integer>> returnCombinationSum(int arr[], int target) {
-        List<List<Integer>> ans = new ArrayList<>();
+    static List<List<Integer>> comboSum(int[] arr, int target){
+        List<List<Integer>> result = new ArrayList<>();
 
-        findCombination(0, arr, target, ans, new ArrayList<>());
-        return ans;
+        findCombination(0, arr, target, new ArrayList<>(), result);
+
+        return result;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 2, 3, 6, 7 };
+        int[] candidates = {2, 3, 6, 7};
+
         int target = 7;
 
-        System.out.println(returnCombinationSum(arr, target));
+        System.out.println(comboSum(candidates, target).toString());
     }
 }
+
+    
