68dafaf90e643d545331bd6cfec58c7d3c7bb6ed
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-03-18 00:10:15 +0530

    *

--- Recursion_BackTracking/Q1_PrintAllCombinations.java
+++ Recursion_BackTracking/Q1_PrintAllCombinations.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 class Q1_PrintAllCombination{
 
+    // for Array
     static void findPermutation(int index, int[] nums, List<List<Integer>> result){
         List<Integer> ds = new ArrayList<>();
         if(index == nums.length){
@@ -29,6 +30,32 @@ class Q1_PrintAllCombination{
         nums[j] = a;
     }
 
+    // for String
+    static void findPermutation(int index, char[] charArr, List<String> res){
+        if(index == charArr.length){
+            String permStr = "";
+            for(int i = 0; i< charArr.length; i++){
+                permStr = permStr + charArr[i];
+            }
+
+            res.add(permStr);
+            return;
+        }
+
+        for(int i = index; i< charArr.length; i++){
+            swap(i, index, charArr);
+            findPermutation(index + 1, charArr, res);
+            swap(i, index, charArr);
+        }
+    }
+
+    static void swap(int i, int j, char[] charArr){
+        char a = charArr[i];
+        charArr[i] = charArr[j];
+        charArr[j] = a;
+    }
+
+
     static List<List<Integer>> permute(int[] nums){
         List<List<Integer>> result = new ArrayList<>();
 
@@ -36,8 +63,21 @@ class Q1_PrintAllCombination{
 
         return result;
     }
+
+    static List<String> permute(String s){
+        List<String> res = new ArrayList<>();
+
+        char[] charArr = s.toCharArray();
+
+        findPermutation(0, charArr, res);
+
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,2,3};
+        String s = "abc";
         System.out.println(permute(nums));
+        System.out.println(permute(s));
     }
 }
