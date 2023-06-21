aba0d0a960dd52dfa23c91b5dc7d9c226e542096
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-06-21 20:04:31 +0530

    *

--- Trees/PostOrderTraversal.java
+++ Trees/PostOrderTraversal.java
@@ -18,9 +18,9 @@ public class PostOrderTraversal {
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        root.left.right.left = new Node(8);
-        root.right.left = new Node(6);
-        root.right.right = new Node(7);
+        root.left.right.left = new Node(6);
+        root.right.left = new Node(7);
+        root.right.right = new Node(8);
         root.right.right.left = new Node(9);
         root.right.right.right = new Node(10);
 
