2ddb075691a2557432152e0859fc8fa6d1f45493
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-06-29 19:33:47 +0530

    *

---
+++ Trees/Q10_VerticalOrder.java
@@ -0,0 +1,81 @@
+package Trees;
+
+import java.util.*;
+
+class Tuple {
+    TreeNode node;
+    int row, col;
+
+    public Tuple(TreeNode _node, int x, int y) {
+        this.node = _node;
+        this.row = x;
+        this.col = y;
+    }
+}
+
+public class Q10_VerticalOrder {
+
+    public static List<List<Integer>> findVertical(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Tuple> q = new LinkedList<Tuple>();
+
+        q.offer(new Tuple(root, 0, 0));
+        
+        while (!q.isEmpty()) {
+            Tuple tuple = q.poll();
+            TreeNode node = tuple.node;
+            int x = tuple.row;
+            int y = tuple.col;
+
+            if (!map.containsKey(x)) {
+                map.put(x, new TreeMap<>());
+            }
+            if (!map.get(x).containsKey(y)) {
+                map.get(x).put(y, new PriorityQueue<>());
+            }
+            map.get(x).get(y).offer(node.data);
+
+            if (node.left != null) {
+                q.offer(new Tuple(node.left, x - 1, y + 1));
+            }
+            if (node.right != null) {
+                q.offer(new Tuple(node.right, x + 1, y + 1));
+            }
+        }
+        List<List<Integer>> list = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            list.add(new ArrayList<>());
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    list.get(list.size() - 1).add(nodes.poll());
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String args[]) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(10);
+        root.left.left.right = new TreeNode(5);
+        root.left.left.right.right = new TreeNode(6);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(9);
+        root.right.right = new TreeNode(10);
+
+        List<List<Integer>> list = new ArrayList<>();
+        list = findVertical(root);
+
+        System.out.println("The Vertical Traversal is : ");
+        for (List<Integer> it : list) {
+            for (int nodeVal : it) {
+                System.out.print(nodeVal + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
