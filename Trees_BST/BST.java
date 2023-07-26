c36a8da1a670c33975246a823dcf115c9f36cbe7
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-07-26 13:38:39 +0530

    successor

--- Trees_BST/BST.java
+++ Trees_BST/BST.java
@@ -4,7 +4,8 @@ class TreeNode {
     int data;
     TreeNode left, right;
 
-    public TreeNode(){}
+    public TreeNode() {
+    }
 
     public TreeNode(int key) {
         this.data = key;
@@ -19,119 +20,41 @@ class TreeNode {
 }
 
 public class BST {
-    public static class Node {
-        int data;
-        Node left;
-        Node right;
+    static TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
 
-        public Node(int data) {
-            // Assign data to the new node, set left and right children to null
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
+        TreeNode curr = root;
 
-    // Represent the root of binary tree
-    public Node root;
+        while (true) {
 
-    public BST() {
-        root = null;
-    }
+            // if current value is less than val assign to right
+            if (curr.data <= val) {
 
-    // insert() will add new node to the binary search tree
-    public void insert(int data) {
-        // Create a new node
-        Node newNode = new Node(data);
-
-        // Check whether tree is empty
-        if (root == null) {
-            root = newNode;
-            return;
-        } else {
-            // current node point to root of the tree
-            Node current = root, parent = null;
-
-            while (true) {
-                // parent keep track of the parent node of current node.
-                parent = current;
-
-                // If data is less than current's data, node will be inserted to the left of
-                // tree
-                if (data < current.data) {
-                    current = current.left;
-                    if (current == null) {
-                        parent.left = newNode;
-                        return;
-                    }
-                }
-                // If data is greater than current's data, node will be inserted to the right of
-                // tree
-                else {
-                    current = current.right;
-                    if (current == null) {
-                        parent.right = newNode;
-                        return;
-                    }
+                // if there is a right then move to right
+                // else create a right and assign val
+                if (curr.right != null) {
+                    curr = curr.right;
+                } else {
+                    curr.right = new TreeNode(val);
+                    break;
                 }
             }
-        }
-    }
 
-    // minNode() will find out the minimum node
-    public Node minNode(Node root) {
-        if (root.left != null)
-            return minNode(root.left);
-        else
-            return root;
-    }
-
-    // deleteNode() will delete the given node from the binary search tree
-    public Node deleteNode(Node node, int value) {
-        if (node == null) {
-            return null;
-        } else {
-            // value is less than node's data then, search the value in left subtree
-            if (value < node.data)
-                node.left = deleteNode(node.left, value);
-
-            // value is greater than node's data then, search the value in right subtree
-            else if (value > node.data)
-                node.right = deleteNode(node.right, value);
-
-            // If value is equal to node's data that is, we have found the node to be
-            // deleted
+            // if current value is greater than val assign to left
             else {
-                // If node to be deleted has no child then, set the node to null
-                if (node.left == null && node.right == null)
-                    node = null;
-
-                // If node to be deleted has only one right child
-                else if (node.left == null) {
-                    node = node.right;
-                }
-
-                // If node to be deleted has only one left child
-                else if (node.right == null) {
-                    node = node.left;
-                }
-
-                // If node to be deleted has two children node
-                else {
-                    // then find the minimum node from right subtree
-                    Node temp = minNode(node.right);
-                    // Exchange the data between node and temp
-                    node.data = temp.data;
-                    // Delete the node duplicate node from right subtree
-                    node.right = deleteNode(node.right, temp.data);
+                if (curr.left != null) {
+                    curr = curr.left;
+                } else {
+                    curr.left = new TreeNode(val);
+                    break;
                 }
             }
-            return node;
         }
+        return root;
     }
 
-    // inorder() will perform inorder traversal on binary search tree
-    public void inorderTraversal(Node node) {
+    public static void inorderTraversal(TreeNode root) {
 
         // Check whether tree is empty
         if (root == null) {
@@ -139,44 +62,26 @@ public class BST {
             return;
         } else {
 
-            if (node.left != null)
-                inorderTraversal(node.left);
-            System.out.print(node.data + " ");
-            if (node.right != null)
-                inorderTraversal(node.right);
+            if (root.left != null)
+                inorderTraversal(root.left);
+            System.out.print(root.data + " ");
+            if (root.right != null)
+                inorderTraversal(root.right);
 
         }
+
     }
 
     public static void main(String[] args) {
+        TreeNode root = null;
 
-        BST bt = new BST();
-        // Add nodes to the binary tree
-        bt.insert(50);
-        bt.insert(30);
-        bt.insert(70);
-        bt.insert(60);
-        bt.insert(10);
-        bt.insert(90);
+        int[] arr = { 5, 2, 4, 3, 1, 8, 9, 10, 7, 6 };
 
-        System.out.println("Binary search tree after insertion:");
-        // Displays the binary tree
-        bt.inorderTraversal(bt.root);
+        for (int a : arr) {
+            root = insertIntoBST(root, a);
+        }
 
-        Node deletedNode = null;
-        // Deletes node 90 which has no child
-        deletedNode = bt.deleteNode(bt.root, 90);
-        System.out.println("\nBinary search tree after deleting node 90:");
-        bt.inorderTraversal(bt.root);
-
-        // Deletes node 30 which has one child
-        deletedNode = bt.deleteNode(bt.root, 30);
-        System.out.println("\nBinary search tree after deleting node 30:");
-        bt.inorderTraversal(bt.root);
-
-        // Deletes node 50 which has two children
-        deletedNode = bt.deleteNode(bt.root, 50);
-        System.out.println("\nBinary search tree after deleting node 50:");
-        bt.inorderTraversal(bt.root);
+        inorderTraversal(root);
     }
+
 }
--- Trees_BST/Q11_InOrderSuccessor.java
+++ Trees_BST/Q11_InOrderSuccessor.java
@@ -3,9 +3,11 @@ package Trees_BST;
 public class Q11_InOrderSuccessor {
     static TreeNode findInOrderSuccessor(TreeNode root, TreeNode p) {
         TreeNode successor = null;
+        TreeNode predecessor = null;
 
         while (root != null) {
             if (p.data >= root.data) {
+                predecessor = root;
                 root = root.right;
             } else {
                 successor = root;
@@ -13,6 +15,77 @@ public class Q11_InOrderSuccessor {
             }
         }
 
+        // System.out.println(predecessor.data + " " + successor.data);
+
         return successor;
+    }
+
+    static TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null)
+            return new TreeNode(val);
+
+        TreeNode curr = root;
+
+        while (true) {
+
+            // if current value is less than val assign to right
+            if (curr.data <= val) {
+
+                // if there is a right then move to right
+                // else create a right and assign val
+                if (curr.right != null) {
+                    curr = curr.right;
+                } else {
+                    curr.right = new TreeNode(val);
+                    break;
+                }
+            }
+
+            // if current value is greater than val assign to left
+            else {
+                if (curr.left != null) {
+                    curr = curr.left;
+                } else {
+                    curr.left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+
+    public static void inorderTraversal(TreeNode root) {
+
+        // Check whether tree is empty
+        if (root == null) {
+            System.out.println("Tree is empty");
+            return;
+        } else {
+
+            if (root.left != null)
+                inorderTraversal(root.left);
+            System.out.print(root.data + " ");
+            if (root.right != null)
+                inorderTraversal(root.right);
+
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = null;
+
+        int[] arr = { 5,2,4,3,1,8,9,10,7,6 };
+
+        for (int a : arr) {
+            root = insertIntoBST(root, a);
+        }
+
+        inorderTraversal(root);
+
+        System.out.println();
+        findInOrderSuccessor(root, root);
+
     }
 }
