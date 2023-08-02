6255c5e284e482bda1e88cc7183aacdbc2a99502
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-08-02 13:28:09 +0530

    BFS done

--- Graphs/Q1_BFSGraph.java
+++ Graphs/Q1_BFSGraph.java
@@ -3,20 +3,21 @@ package Graphs;
 import java.util.*;
 
 public class Q1_BFSGraph {
-    public ArrayList<Integer> bfsTraversal(int N, ArrayList<ArrayList<Integer>> adj){
+    public static ArrayList<Integer> bfsTraversal(int N, ArrayList<ArrayList<Integer>> adj) {
         ArrayList<Integer> bfs = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
         boolean[] isVisited = new boolean[N];
 
-        q.add(0);
-        isVisited[0] = true;
+        // starting node is 1
+        q.add(1);
+        isVisited[1] = true;
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Integer node = q.poll();
             bfs.add(node);
 
-            for(Integer it: adj.get(node)){
-                if(isVisited[it] == false){
+            for (Integer it : adj.get(node)) {
+                if (isVisited[it] == false) {
                     isVisited[it] = true;
                     q.add(it);
                 }
@@ -25,7 +26,81 @@ public class Q1_BFSGraph {
 
         return bfs;
     }
+
+    public static ArrayList<Integer> bfsTraversalMatrix(int N, int[][] adj) {
+        ArrayList<Integer> bfs = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] isVisited = new boolean[adj.length];
+
+        // starting node is 1
+        q.add(1);
+        isVisited[1] = true;
+
+        while (!q.isEmpty()) {
+            Integer node = q.poll();
+            bfs.add(node);
+
+            for (int i = 0; i < adj.length; i++) {
+                if (adj[node][i] == 1 && !isVisited[i]) {
+                    isVisited[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+
+        return bfs;
+    }
+
+    static void addAdjList(int u, int v, ArrayList<ArrayList<Integer>> adjList) {
+        adjList.get(u).add(v);
+        adjList.get(v).add(u);
+    }
+
+    static void addUnDirectedMatrix(int u, int v, int[][] adj) {
+        // for undirected graph - do adj[u][v], do adj[v][u]
+        // edge 1 --- 2
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        int n = 9;
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        int[][] adjMatrix = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= 9; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        // adding adjacency List
+        addAdjList(1, 2, adjList);
+        addAdjList(1, 6, adjList);
+        addAdjList(2, 3, adjList);
+        addAdjList(2, 4, adjList);
+        addAdjList(6, 7, adjList);
+        addAdjList(6, 9, adjList);
+        addAdjList(4, 5, adjList);
+        addAdjList(7, 8, adjList);
+        addAdjList(5, 8, adjList);
+
+        // adding adjacency Matrix
+        addUnDirectedMatrix(1, 2, adjMatrix);
+        addUnDirectedMatrix(1, 6, adjMatrix);
+        addUnDirectedMatrix(2, 3, adjMatrix);
+        addUnDirectedMatrix(2, 4, adjMatrix);
+        addUnDirectedMatrix(6, 7, adjMatrix);
+        addUnDirectedMatrix(6, 9, adjMatrix);
+        addUnDirectedMatrix(4, 5, adjMatrix);
+        addUnDirectedMatrix(7, 8, adjMatrix);
+        addUnDirectedMatrix(5, 8, adjMatrix);
+
+        System.out.println("Adjacency List : " + adjList);
+
+        // n+1 vertexes
+        System.out.println("BFS : " + bfsTraversal(n + 1, adjList));
+        
+        System.out.println("BFS : " + bfsTraversalMatrix(n + 1, adjMatrix));
+
     }
 }
