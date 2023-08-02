cecd5ca3509af8daeefb1e10c7e01dbc4d64b218
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-08-02 14:11:46 +0530

    dfs

---
+++ Graphs/Q2_DFS_Graph.java
@@ -0,0 +1,53 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class Q2_DFS_Graph {
+
+    static void dfsHelper(int node, boolean[] isVisited,  ArrayList<ArrayList<Integer>> adjList, ArrayList<Integer> bfs){
+        isVisited[node] = true;
+        bfs.add(node);
+
+        for(Integer it: adjList.get(node)){
+            if(!isVisited[it]){
+                dfsHelper(it, isVisited, adjList, bfs);
+            }
+        }
+    }
+    static ArrayList<Integer> dfs(int V, ArrayList<ArrayList<Integer>> adjList){
+        boolean[] isVisited = new boolean[V+1];
+        ArrayList<Integer> bfs = new ArrayList<>();
+
+        // 0 based indexing
+        isVisited[0] = true;
+
+        dfsHelper(0, isVisited, adjList, bfs);
+        return bfs;
+    }
+
+    static void addAdjList(int u, int v, ArrayList<ArrayList<Integer>> adjList) {
+        adjList.get(u).add(v);
+        adjList.get(v).add(u);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+
+        // because of 0 based indexing <
+        for (int i = 0; i < 5; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        // adding adjacency List
+        addAdjList(0, 1, adjList);
+        addAdjList(0, 2, adjList);
+        addAdjList(0, 4, adjList);
+        addAdjList(4, 3, adjList);
+
+        System.out.println(adjList);
+
+        System.out.println(dfs(n, adjList));
+        
+    }
+}
