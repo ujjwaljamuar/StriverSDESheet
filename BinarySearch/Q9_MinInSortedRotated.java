cd3fad89b851fae66c9e12b690957577a3d8fe95
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-10 23:04:30 +0530

    minimum value in sorted rotated array

---
+++ BinarySearch/Q9_MinInSortedRotated.java
@@ -0,0 +1,27 @@
+class Q9_MinInSortedRotated{
+    int findMin(int[] arr){
+        int low = 0, mid, high = arr.length - 1, ans = Integer.MAX_VALUE;
+
+        while(low <= high){
+            mid = low + ((high - low)/2);
+            
+            if(arr[low] <= arr[mid]){
+                ans = Math.min(ans, arr[low]);
+                low = mid + 1;
+            }
+            else{
+                ans = Math.min(ans, arr[mid]);
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[] = {25, 30, 5, 10, 15, 20};
+
+        Q9_MinInSortedRotated bs = new Q9_MinInSortedRotated();
+
+        System.out.println(bs.findMin(arr));
+    }
+}
