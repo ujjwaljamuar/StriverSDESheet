d5dea6d0e7572992644d2ca604eca9250eececd4
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2024-03-11 23:17:23 +0530

    min in sorted rotated array

--- BinarySearch/Q9_MinInSortedRotated.java
+++ BinarySearch/Q9_MinInSortedRotated.java
@@ -1,15 +1,19 @@
-class Q9_MinInSortedRotated{
-    int findMin(int[] arr){
+class Q9_MinInSortedRotated {
+    int findMin(int[] arr) {
         int low = 0, mid, high = arr.length - 1, ans = Integer.MAX_VALUE;
 
-        while(low <= high){
-            mid = low + ((high - low)/2);
-            
-            if(arr[low] <= arr[mid]){
+        while (low <= high) {
+            mid = low + ((high - low) / 2);
+
+            if (arr[low] <= arr[high]) {
+                ans = Math.min(ans, arr[low]);
+                break;
+            }
+
+            if (arr[low] <= arr[mid]) {
                 ans = Math.min(ans, arr[low]);
                 low = mid + 1;
-            }
-            else{
+            } else {
                 ans = Math.min(ans, arr[mid]);
                 high = mid - 1;
             }
@@ -17,15 +21,15 @@ class Q9_MinInSortedRotated{
 
         return ans;
     }
+
     public static void main(String[] args) {
-        int arr[] = {25, 30, 5, 10, 15, 20};
+        int arr[] = { 25, 30, 5, 10, 15, 20 };
 
         Q9_MinInSortedRotated bs = new Q9_MinInSortedRotated();
 
         System.out.println(bs.findMin(arr));
     }
 }
-
 
 // take a max value
 // check which part is sorted
