3bed3a328b9dd677582e10f1ad164db0d55c487b
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-03-28 18:40:46 +0530

    bfs traversal

--- Graphs/Q1_BFSGraph.java
+++ Graphs/Q1_BFSGraph.java
@@ -3,104 +3,52 @@ package Graphs;
 import java.util.*;
 
 public class Q1_BFSGraph {
-    public static ArrayList<Integer> bfsTraversal(int N, ArrayList<ArrayList<Integer>> adj) {
+    ArrayList<Integer> bfsTraversal(int n, ArrayList<ArrayList<Integer>> adjList) {
         ArrayList<Integer> bfs = new ArrayList<>();
+        boolean[] isVisited = new boolean[n];
+
         Queue<Integer> q = new LinkedList<>();
-        boolean[] isVisited = new boolean[adj.size()];
 
-        // starting node is 1
-        q.add(1);
-        isVisited[1] = true;
+        // add starting node to queue and mark it visited
+        q.add(0);
+        isVisited[0] = true;
 
+        // till queue is not empty
         while (!q.isEmpty()) {
+            // take out the first element in queue and add it to bfs list
             Integer node = q.poll();
             bfs.add(node);
 
-            for (Integer it : adj.get(node)) {
-                if (isVisited[it] == false) {
-                    isVisited[it] = true;
-                    q.add(it);
-                }
-            }
-        }
-
-        return bfs;
-    }
-
-    public static ArrayList<Integer> bfsTraversalMatrix(int N, int[][] adj) {
-        ArrayList<Integer> bfs = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] isVisited = new boolean[adj.length];
-
-        // starting node is 1
-        q.add(1);
-        isVisited[1] = true;
-
-        while (!q.isEmpty()) {
-            Integer node = q.poll();
-            bfs.add(node);
-
-            for (int i = 0; i < adj.length; i++) {
-                if (adj[node][i] == 1 && !isVisited[i]) {
+            for (Integer i : adjList.get(node)) {
+                // check if the element is already visited, if not mark it and add it to queue
+                // to be visited
+                if (isVisited[i] == false) {
                     isVisited[i] = true;
                     q.add(i);
                 }
             }
         }
-
         return bfs;
     }
 
-    static void addAdjList(int u, int v, ArrayList<ArrayList<Integer>> adjList) {
-        adjList.get(u).add(v);
-        adjList.get(v).add(u);
-    }
-
-    static void addUnDirectedMatrix(int u, int v, int[][] adj) {
-        // for undirected graph - do adj[u][v], do adj[v][u]
-        // edge 1 --- 2
-        adj[u][v] = 1;
-        adj[v][u] = 1;
-    }
-
     public static void main(String[] args) {
+        Q1_BFSGraph graph = new Q1_BFSGraph();
 
-        int n = 9;
         ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-        int[][] adjMatrix = new int[n + 1][n + 1];
 
-        for (int i = 0; i <= 9; i++) {
+        for (int i = 0; i < 5; i++) {
             adjList.add(new ArrayList<>());
         }
+        adjList.get(0).add(1);
+        adjList.get(1).add(0);
+        adjList.get(0).add(4);
+        adjList.get(4).add(0);
+        adjList.get(1).add(2);
+        adjList.get(2).add(1);
+        adjList.get(1).add(3);
+        adjList.get(3).add(1);
 
-        // adding adjacency List
-        addAdjList(1, 2, adjList);
-        addAdjList(1, 6, adjList);
-        addAdjList(2, 3, adjList);
-        addAdjList(2, 4, adjList);
-        addAdjList(6, 7, adjList);
-        addAdjList(6, 9, adjList);
-        addAdjList(4, 5, adjList);
-        addAdjList(7, 8, adjList);
-        addAdjList(5, 8, adjList);
-
-        // adding adjacency Matrix
-        addUnDirectedMatrix(1, 2, adjMatrix);
-        addUnDirectedMatrix(1, 6, adjMatrix);
-        addUnDirectedMatrix(2, 3, adjMatrix);
-        addUnDirectedMatrix(2, 4, adjMatrix);
-        addUnDirectedMatrix(6, 7, adjMatrix);
-        addUnDirectedMatrix(6, 9, adjMatrix);
-        addUnDirectedMatrix(4, 5, adjMatrix);
-        addUnDirectedMatrix(7, 8, adjMatrix);
-        addUnDirectedMatrix(5, 8, adjMatrix);
-
-        System.out.println("Adjacency List : " + adjList);
-
-        // n+1 vertexes because of 1 based indexing
-        System.out.println("BFS : " + bfsTraversal(n + 1, adjList));
-
-        System.out.println("BFS : " + bfsTraversalMatrix(n + 1, adjMatrix));
-
+        System.out.println(graph.bfsTraversal(5, adjList));
     }
+
 }
