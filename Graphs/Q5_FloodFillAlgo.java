f960afd6e4810692454d3631361e37f15ac8c695
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-04-02 23:38:20 +0530

    flood fill algorithm

--- Graphs/Q5_FloodFillAlgo.java
+++ Graphs/Q5_FloodFillAlgo.java
@@ -2,41 +2,40 @@ package Graphs;
 
 public class Q5_FloodFillAlgo {
 
-    static int[][] floodFill(int sr, int sc, int[][] image, int newColor) {
-        int initialColor = image[sr][sc];
-        int[][] ans = image;
-
-        int[] delrow = { -1, 0, 1, 0 };
-        int[] delcol = { 0, 1, 0, -1 };
-
-        dfs(sr, sc, ans, image, delrow, delcol, initialColor, newColor);
-
-        return ans;
-    }
-
-    static void dfs(int row, int col, int[][] ans, int[][] image, int[] delrow, int[] delcol, int initialColor,
-            int newColor) {
+    private void dfs(int row, int col, int[][] ans, int[][] image, int newColor, int[] delrow, int[] delCol,
+            int initialColor) {
         ans[row][col] = newColor;
+        int n = image.length;
+        int m = image[0].length;
 
-        int n = ans.length;
-        int m = ans[0].length;
-
-        // delrow and col are exactly 4
         for (int i = 0; i < 4; i++) {
-            // find neighbours
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
+            int nRow = row + delrow[i];
+            int nCol = col + delCol[i];
 
-            // perform checks
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == initialColor
-                    && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, delrow, delcol, initialColor, newColor);
+            // check for neighbour is valid or not
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
+                    image[nRow][nCol] == initialColor && ans[nRow][nCol] != newColor) {
+                dfs(nRow, nCol, ans, image, newColor, delrow, delCol, initialColor);
             }
         }
 
     }
 
+    int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int initialColor = image[sr][sc];
+        int[][] ans = image;
+
+        int[] delrow = { -1, 0, 1, 0 };
+        int[] delCol = { 0, 1, 0, -1 };
+
+        dfs(sr, sc, ans, image, newColor, delrow, delCol, initialColor);
+
+        return ans;
+    }
+
     public static void main(String[] args) {
+        Q5_FloodFillAlgo graph = new Q5_FloodFillAlgo();
+
         int[][] image = {
                 { 1, 1, 1 },
                 { 1, 1, 0 },
@@ -44,7 +43,7 @@ public class Q5_FloodFillAlgo {
         };
 
         // sr = 1, sc = 1, newColor = 2
-        int[][] ans = floodFill(1, 1, image, 2);
+        int[][] ans = graph.floodFill(image, 1, 1, 2);
         for (int i = 0; i < ans.length; i++) {
             for (int j = 0; j < ans[i].length; j++)
                 System.out.print(ans[i][j] + " ");
