08f59d4f82248e61f6c9f72826a2e4f7ad9089f8
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-04-10 15:43:52 +0530

    number of enclaves

--- Graphs/Q11_NumberOfEnclaves.java
+++ Graphs/Q11_NumberOfEnclaves.java
@@ -4,6 +4,55 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Q11_NumberOfEnclaves {
+    // using dfs - always fast
+
+    int noOfEnclaves(int[][] Matrix) {
+        int n = Matrix.length;
+        int m = Matrix[0].length;
+        boolean[][] isVisited = new boolean[n][m];
+
+        int delRow[] = { -1, 0, +1, 0 };
+        int delCol[] = { 0, +1, +0, -1 };
+
+        // for all boundaries
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
+                    if (Matrix[i][j] == 1) {
+                        isVisited[i][j] = true;
+                        dfs(i, j, isVisited, Matrix, n, m, delRow, delCol);
+                    }
+                }
+            }
+        }
+
+        // count remaining 1s
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (Matrix[i][j] == 1 && !isVisited[i][j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    void dfs(int row, int col, boolean[][] isVisited, int[][] Matrix, int n, int m, int[] delRow, int[] delCol) {
+        isVisited[row][col] = true;
+
+        // for neighbours
+        for (int i = 0; i < 4; i++) {
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && !isVisited[nRow][nCol] && Matrix[nRow][nCol] == 1) {
+                dfs(nRow, nCol, isVisited, Matrix, n, m, delRow, delCol);
+            }
+        }
+    }
+
+    /// using bfs
     int numberOfEnclaves(int[][] Matrix) {
         int n = Matrix.length;
         int m = Matrix[0].length;
@@ -55,7 +104,6 @@ public class Q11_NumberOfEnclaves {
                 }
             }
         }
-
         return count;
     }
 
@@ -69,7 +117,11 @@ public class Q11_NumberOfEnclaves {
                 { 0, 0, 0, 0 } };
 
         int ans = graph.numberOfEnclaves(grid);
+        int ans2 = graph.noOfEnclaves(grid);
+
         System.out.println(ans);
+        System.out.println(ans2);
+
     }
 
     class Pair {
@@ -84,10 +136,10 @@ public class Q11_NumberOfEnclaves {
 }
 
 /*
- Start with the boundary elements
- both bfs and dfs can be used
- use bfs
- mark visited to all neighbours
- after bfs if there any remains 
- count and return
+ * Start with the boundary elements
+ * both bfs and dfs can be used
+ * use bfs
+ * mark visited to all neighbours
+ * after bfs if there any remains
+ * count and return
  */
