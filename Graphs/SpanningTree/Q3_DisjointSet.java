3722565e93b28a69637c212154c43138715013d2
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-06-19 16:06:21 +0530

    disjoint sets union by rank

--- Graphs/SpanningTree/Q3_DisjointSet.java
+++ Graphs/SpanningTree/Q3_DisjointSet.java
@@ -3,6 +3,51 @@ package SpanningTree;
 import java.util.ArrayList;
 
 public class Q3_DisjointSet {
+    private static class DisjointSet {
+        ArrayList<Integer> rank = new ArrayList<>();
+        ArrayList<Integer> parent = new ArrayList<>();
+
+        DisjointSet(int n) {
+            for (int i = 0; i <= n; i++) {
+                rank.add(0);
+                parent.add(i);
+            }
+        }
+
+        int findUPar(int node) {
+            if (node == parent.get(node)) {
+                return node;
+            }
+
+            parent.set(node, findUPar(parent.get(node)));
+            return parent.get(node);
+        }
+
+        void unionByRank(int u, int v) {
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+
+            if (ulp_u == ulp_v) {
+                return;
+            }
+
+            if (rank.get(ulp_u) < rank.get(ulp_v)) {
+                parent.set(ulp_u, ulp_v);
+            }
+
+            else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+                parent.set(ulp_v, ulp_u);
+            }
+
+            else {
+                parent.set(ulp_v, ulp_u);
+                int rankU = rank.get(ulp_u);
+                rank.set(ulp_u, rankU + 1);
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         DisjointSet ds = new DisjointSet(7);
 
@@ -27,49 +72,4 @@ public class Q3_DisjointSet {
         }
     }
 
-    private static class DisjointSet {
-        ArrayList<Integer> rank = new ArrayList<>();
-        ArrayList<Integer> parent = new ArrayList<>();
-    
-        DisjointSet(int n) {
-            for (int i = 0; i <= n; i++) {
-                rank.add(0);
-                parent.add(i);
-            }
-        }
-    
-        int findUPar(int node) {
-            if (node == parent.get(node)) {
-                return node;
-            }
-    
-            parent.set(node, findUPar(parent.get(node)));
-            return parent.get(node);
-        }
-    
-        void unionByRank(int u, int v) {
-            int ulp_u = findUPar(u);
-            int ulp_v = findUPar(v);
-    
-            if (ulp_u == ulp_v) {
-                return;
-            }
-    
-            if (rank.get(ulp_u) < rank.get(ulp_v)) {
-                parent.set(ulp_u, ulp_v);
-            }
-    
-            else if (rank.get(ulp_v) < rank.get(ulp_u)) {
-                parent.set(ulp_v, ulp_u);
-            }
-    
-            else {
-                parent.set(ulp_v, ulp_u);
-                int rankU = rank.get(ulp_u);
-                rank.set(ulp_u, rankU + 1);
-            }
-        }
-    
-    }
 }
-
