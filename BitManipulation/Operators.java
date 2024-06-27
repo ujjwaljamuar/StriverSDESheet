9d25730f46be048e0e88c943656150d06ceb045b
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-06-27 19:34:10 +0530

    operators

---
+++ BitManipulation/Operators.java
@@ -0,0 +1,48 @@
+public class Operators {
+    /*
+
+    # AND &
+    all true - true
+    1 false - false
+
+    # OR |
+    1 true - true
+    all false - false
+
+    # XOR ^
+
+    no of 1s odd - 1
+    no of 1s even - 1
+
+
+    # Right Shift >> 1 
+    n/ 2pow, rightShift
+
+    14>> 1 = 14/2pow1
+
+    
+    # Left Shift << 1
+    n * 2 pow leftShift
+    
+    14 << 1 = 14 * 2pow1
+
+    
+    last bit is reserved for sign
+
+    # 2s compliment
+    - do 1s compliment ie flip all bits
+    - then add 1
+
+    # NOT ~
+
+    0 - 1
+    1 - 0
+
+    after performing not check
+    - if negative
+        do 2s compliment
+    - if positive
+        stop
+
+     */
+}
