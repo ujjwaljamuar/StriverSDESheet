437db46b3086fdbb02d5e7da5e6c9bb4be364415
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-06-30 15:36:28 +0530

    check ith bit is set or not

---
+++ BitManipulation/Questions/CheckIthBitSet.java
@@ -0,0 +1,14 @@
+package Questions;
+
+public class CheckIthBitSet {
+    public static void main(String[] args) {
+        int n = 9, i = 2;
+
+        if((n & (1 << i)) != 0){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}
