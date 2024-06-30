f2e3491800370ba58589245c1ea86d78bb61b548
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-06-30 16:07:05 +0530

    toggle ith bit

---
+++ BitManipulation/Questions/Q5_ToggleIthBit.java
@@ -0,0 +1,10 @@
+package Questions;
+
+public class Q5_ToggleIthBit {
+    public static void main(String[] args) {
+        int n = 13, i = 1;
+        // answer will be 15
+
+        System.out.println(n ^ (1 << i));
+    }
+}
