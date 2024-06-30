bca8e589b352370b7918ea285f80f0f395147e2d
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-06-30 15:17:30 +0530

    Swap Two Numbers

---
+++ BitManipulation/Questions/SwapNumbers.java
@@ -0,0 +1,14 @@
+package Questions;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        System.out.println("a = " + a + ", b = " + b);
+
+        a = a ^ b;
+        b = a ^ b;    // a ^ b ^ b, so b ^ b will be 0
+        a = a ^ b;
+
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
