75f38362dd3cb50af0cd94034d3635190dd6d2f9
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-09-30 20:06:55 +0530

    count max num of platforms for trains optimized soln

--- Greedy/Q8_MinPlatforms.java
+++ Greedy/Q8_MinPlatforms.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Q8_MinPlatforms {
 
     int countPlatformsBrute(int n, int[] arrival, int[] dept) {
@@ -18,13 +20,33 @@ public class Q8_MinPlatforms {
         return maxPlatform;
     }
 
+    int countPlatforms(int n, int[] arrival, int[] dept) {
+        int maxPlatform = 0, i = 0, j = 0, count = 0;
+        Arrays.sort(arrival);
+        Arrays.sort(dept);
+
+        while (i < n) {
+            if (arrival[i] <= dept[j]) {
+                count++;
+                i += 1;
+            } else {
+                count--;
+                j += 1;
+            }
+
+            maxPlatform = Math.max(maxPlatform, count);
+        }
+
+        return maxPlatform;
+    }
+
     public static void main(String[] args) {
         Q8_MinPlatforms grd = new Q8_MinPlatforms();
 
         int[] arr = { 900, 945, 955, 1100, 1500, 1800 };
         int[] dep = { 920, 1200, 1130, 1150, 1900, 2000 };
         int n = arr.length;
-        int totalCount = grd.countPlatformsBrute(n, arr, dep);
+        int totalCount = grd.countPlatforms(n, arr, dep);
         System.out.println("Minimum number of Platforms required " + totalCount);
     }
 }
