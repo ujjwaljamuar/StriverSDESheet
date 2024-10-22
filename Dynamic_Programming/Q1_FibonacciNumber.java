ac3a6c10e435d8d484b494367032dc6e93bb08ff
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-10-23 00:17:22 +0530

    fibonacci optimized

--- Dynamic_Programming/Q1_FibonacciNumber.java
+++ Dynamic_Programming/Q1_FibonacciNumber.java
@@ -2,6 +2,22 @@ import java.util.Arrays;
 
 public class Q1_FibonacciNumber {
 
+    int fibOpt(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int prev2 = 0;
+        int prev = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+
     int fibTab(int n) {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
@@ -42,13 +58,14 @@ public class Q1_FibonacciNumber {
     public static void main(String[] args) {
         Q1_FibonacciNumber dp = new Q1_FibonacciNumber();
 
-        int n = 5;
+        int n = 40;
 
         int[] dpArr = new int[n + 1];
         Arrays.fill(dpArr, -1);
 
         System.out.println(dp.fibRec(n));
         System.out.println(dp.fibMem(n, dpArr));
-        System.err.println(dp.fibTab(n));
+        System.out.println(dp.fibTab(n));
+        System.out.println(dp.fibOpt(n));
     }
 }
