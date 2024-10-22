ca4724abc9d79ef0ea9ff47c1672c739dddf8c3c
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-10-22 23:58:13 +0530

    fibonacci using memoization

--- Dynamic_Programming/Q1_FibonacciNumber.java
+++ Dynamic_Programming/Q1_FibonacciNumber.java
@@ -1,4 +1,14 @@
+import java.util.Arrays;
+
 public class Q1_FibonacciNumber {
+
+    int fibMem(int n, int[] dpArr) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return dpArr[n] = fibMem(n - 1, dpArr) + fibMem(n - 2, dpArr);
+    }
 
     int fibRec(int n) {
         if (n <= 1) {
@@ -13,6 +23,10 @@ public class Q1_FibonacciNumber {
 
         int n = 5;
 
+        int[] dpArr = new int[n + 1];
+        Arrays.fill(dpArr, -1);
+
         System.out.println(dp.fibRec(n));
+        System.out.println(dp.fibMem(n, dpArr));
     }
 }
