3064fe0d3c37b144c9508baca6daeacd8fedccdd
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-10-23 00:07:10 +0530

    fibonacci by tabulation

--- Dynamic_Programming/Q1_FibonacciNumber.java
+++ Dynamic_Programming/Q1_FibonacciNumber.java
@@ -2,6 +2,23 @@ import java.util.Arrays;
 
 public class Q1_FibonacciNumber {
 
+    int fibTab(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        if (n <= 1) {
+            return n;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n];
+    }
+
     int fibMem(int n, int[] dpArr) {
         if (n <= 1) {
             return n;
@@ -32,5 +49,6 @@ public class Q1_FibonacciNumber {
 
         System.out.println(dp.fibRec(n));
         System.out.println(dp.fibMem(n, dpArr));
+        System.err.println(dp.fibTab(n));
     }
 }
