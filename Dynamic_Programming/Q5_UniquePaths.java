d0faea147cb72793b0681b3fdbab62248c54c61e
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-02 20:58:22 +0530

    Number of unique ways in a grid to go from a to b only by right and down using tabulation

--- Dynamic_Programming/Q5_UniquePaths.java
+++ Dynamic_Programming/Q5_UniquePaths.java
@@ -1,6 +1,38 @@
 import java.util.Arrays;
 
 public class Q5_UniquePaths {
+    static int countWaysUtilTab(int m, int n) {
+        int[][] dp = new int[m][n];
+        // Loop through each cell in the grid
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Base condition: If we are at the top-left cell (0, 0), there's one way to
+                // reach it.
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                }
+
+                else {
+                    int up = 0;
+                    int left = 0;
+
+                    // Calculate the number of ways by moving up (if possible) and left (if
+                    // possible)
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    if (j > 0)
+                        left = dp[i][j - 1];
+
+                    // Store the total number of ways to reach the current cell in the DP array
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        // Return the number of ways to reach the bottom-right cell (m-1, n-1)
+        return dp[m - 1][n - 1];
+    }
+
     int countWaysUtilMem(int i, int j, int[][] dp) {
         if (i == 0 && j == 0) {
             return 1;
@@ -47,7 +79,9 @@ public class Q5_UniquePaths {
         for (int[] row : dp)
             Arrays.fill(row, -1);
 
-        return countWaysUtilMem(m - 1, n - 1, dp);
+        // return countWaysUtilMem(m - 1, n - 1, dp);
+
+        return countWaysUtilTab(m, n);
 
     }
 
