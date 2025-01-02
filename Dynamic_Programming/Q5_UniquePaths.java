2c116e64c8110fae4586902294e0a8f25d4edf4f
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-02 21:00:52 +0530

    Number of unique ways in a grid to go from a to b only by right and down using space optimized

--- Dynamic_Programming/Q5_UniquePaths.java
+++ Dynamic_Programming/Q5_UniquePaths.java
@@ -1,7 +1,45 @@
 import java.util.Arrays;
 
 public class Q5_UniquePaths {
-    static int countWaysUtilTab(int m, int n) {
+    int countWaysUtilOpt(int m, int n) {
+        // Create an array to store the results for the previous row
+        int prev[] = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            // Create a temporary array to store the results for the current row
+            int temp[] = new int[n];
+
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    // Base condition: There's one way to reach the top-left cell (0, 0)
+                    temp[j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                // Calculate the number of ways by moving up (if possible) and left (if
+                // possible)
+                if (i > 0)
+                    up = prev[j];
+                if (j > 0)
+                    left = temp[j - 1];
+
+                // Store the total number of ways to reach the current cell in the temporary
+                // array
+                temp[j] = up + left;
+            }
+
+            // Set the temporary array as the previous array for the next row
+            prev = temp;
+        }
+
+        // Return the number of ways to reach the bottom-right cell (m-1, n-1)
+        return prev[n - 1];
+    }
+
+    int countWaysUtilTab(int m, int n) {
         int[][] dp = new int[m][n];
         // Loop through each cell in the grid
         for (int i = 0; i < m; i++) {
@@ -81,7 +119,9 @@ public class Q5_UniquePaths {
 
         // return countWaysUtilMem(m - 1, n - 1, dp);
 
-        return countWaysUtilTab(m, n);
+        // return countWaysUtilTab(m, n);
+
+        return countWaysUtilOpt(m, n);
 
     }
 
