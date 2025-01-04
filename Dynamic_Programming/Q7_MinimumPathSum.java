5d3f22464ab272c64511a0b411320ce72b4279fb
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-04 20:02:15 +0530

    Minimum path sum to reach from A to B in a grid Memoization

--- Dynamic_Programming/Q7_MinimumPathSum.java
+++ Dynamic_Programming/Q7_MinimumPathSum.java
@@ -1,4 +1,25 @@
+import java.util.Arrays;
+
 public class Q7_MinimumPathSum {
+    int findPathSumMem(int i, int j, int[][] grid, int[][] dp) {
+        if (i == 0 && j == 0) {
+            return grid[0][0];
+        }
+
+        if (i < 0 || j < 0) {
+            return (int) Math.pow(10, 9);
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int up = grid[i][j] + findPathSumMem(i - 1, j, grid, dp);
+        int left = grid[i][j] + findPathSumMem(i, j - 1, grid, dp);
+
+        return dp[i][j] = Math.min(up, left);
+    }
+
     int findPathSumRec(int i, int j, int[][] grid) {
         if (i == 0 && j == 0) {
             return grid[0][0];
@@ -19,7 +40,14 @@ public class Q7_MinimumPathSum {
         int m = grid.length;
         int n = grid[0].length;
 
-        return findPathSumRec(m - 1, n - 1, grid);
+        int[][] dp = new int[m][n];
+        for (int[] rows : dp) {
+            Arrays.fill(rows, -1);
+        }
+
+        // return findPathSumRec(m - 1, n - 1, grid);
+
+        return findPathSumMem(m - 1, n - 1, grid, dp);
     }
 
     public static void main(String[] args) {
