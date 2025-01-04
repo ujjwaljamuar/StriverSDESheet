909e414a8fd11741a3455b9d100c85b46c793391
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-04 20:08:18 +0530

    Minimum path sum to reach from A to B in a grid Space Optimized

--- Dynamic_Programming/Q7_MinimumPathSum.java
+++ Dynamic_Programming/Q7_MinimumPathSum.java
@@ -1,6 +1,42 @@
 import java.util.Arrays;
 
 public class Q7_MinimumPathSum {
+    int findPathSumOpt(int n, int m, int[][] matrix) {
+        // Initialize an array to store the previous row values
+        int prev[] = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            // Create a temporary array to store the current row values
+            int temp[] = new int[m];
+
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0)
+                    temp[j] = matrix[i][j]; // If we're at the top-left cell, the minimum sum is its value
+                else {
+                    int up = matrix[i][j];
+                    if (i > 0)
+                        up += prev[j]; // Add the value from above if it's not out of bounds
+                    else
+                        up += (int) Math.pow(10, 9); // Add a large value if out of bounds in the up direction
+
+                    int left = matrix[i][j];
+                    if (j > 0)
+                        left += temp[j - 1]; // Add the value from the left if it's not out of bounds
+                    else
+                        left += (int) Math.pow(10, 9); // Add a large value if out of bounds in the left direction
+
+                    // Store the minimum of the two possible paths in the current cell
+                    temp[j] = Math.min(up, left);
+                }
+            }
+            // Update the previous row with the values of the current row
+            prev = temp;
+        }
+
+        // The final result is stored in the last element of the previous row
+        return prev[m - 1];
+    }
+
     int findPathSumTab(int n, int m, int[][] matrix) {
         int dp[][] = new int[n][m];
 
@@ -80,7 +116,9 @@ public class Q7_MinimumPathSum {
 
         // return findPathSumMem(m - 1, n - 1, grid, dp);
 
-        return findPathSumTab(m , n, grid);
+        // return findPathSumTab(m , n, grid);
+
+        return findPathSumOpt(m , n, grid);
     }
 
     public static void main(String[] args) {
