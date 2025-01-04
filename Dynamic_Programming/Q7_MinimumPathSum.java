995b306c77741df122c7db2ca9ed9e9cb492b144
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-04 20:05:28 +0530

    Minimum path sum to reach from A to B in a grid Tabulation

--- Dynamic_Programming/Q7_MinimumPathSum.java
+++ Dynamic_Programming/Q7_MinimumPathSum.java
@@ -1,6 +1,37 @@
 import java.util.Arrays;
 
 public class Q7_MinimumPathSum {
+    int findPathSumTab(int n, int m, int[][] matrix) {
+        int dp[][] = new int[n][m];
+
+        // Iterate through the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0)
+                    dp[i][j] = matrix[i][j]; // If we're at the top-left cell, the minimum sum is its value
+                else {
+                    int up = matrix[i][j];
+                    if (i > 0)
+                        up += dp[i - 1][j]; // Add the value from above if it's not out of bounds
+                    else
+                        up += (int) Math.pow(10, 9); // Add a large value if out of bounds in the up direction
+
+                    int left = matrix[i][j];
+                    if (j > 0)
+                        left += dp[i][j - 1]; // Add the value from the left if it's not out of bounds
+                    else
+                        left += (int) Math.pow(10, 9); // Add a large value if out of bounds in the left direction
+
+                    // Store the minimum of the two possible paths
+                    dp[i][j] = Math.min(up, left);
+                }
+            }
+        }
+
+        // The final result is stored in the bottom-right cell of the DP matrix
+        return dp[n - 1][m - 1];
+    }
+
     int findPathSumMem(int i, int j, int[][] grid, int[][] dp) {
         if (i == 0 && j == 0) {
             return grid[0][0];
@@ -36,7 +67,7 @@ public class Q7_MinimumPathSum {
         return Math.min(up, left);
     }
 
-    public int minPathSum(int[][] grid) {
+    int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
 
@@ -47,7 +78,9 @@ public class Q7_MinimumPathSum {
 
         // return findPathSumRec(m - 1, n - 1, grid);
 
-        return findPathSumMem(m - 1, n - 1, grid, dp);
+        // return findPathSumMem(m - 1, n - 1, grid, dp);
+
+        return findPathSumTab(m , n, grid);
     }
 
     public static void main(String[] args) {
