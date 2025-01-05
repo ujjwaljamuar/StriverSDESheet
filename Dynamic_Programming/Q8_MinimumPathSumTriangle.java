94be17f0c50413245d8023a77001bf6b14e9f330
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-05 20:59:39 +0530

    minimum path sum in a triangular array tabulation

--- Dynamic_Programming/Q8_MinimumPathSumTriangle.java
+++ Dynamic_Programming/Q8_MinimumPathSumTriangle.java
@@ -2,6 +2,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Q8_MinimumPathSumTriangle {
+    int findMinPathSumTab(List<List<Integer>> triangle, int n) {
+        // Create a 2D array to store intermediate results
+        int dp[][] = new int[n][n];
+
+        // Initialize the bottom row of dp with the values from the bottom row of the
+        // triangle
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = triangle.get(n - 1).get(j);
+        }
+
+        // Starting from the second to last row, calculate the minimum path sum for each
+        // element
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                // Calculate the two possible paths: moving down or moving diagonally
+                int down = triangle.get(i).get(j) + dp[i + 1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i + 1][j + 1];
+
+                // Store the minimum of the two paths in dp
+                dp[i][j] = Math.min(down, diagonal);
+            }
+        }
+
+        // The result is stored at the top of dp array
+        return dp[0][0];
+    }
+
     int findMinPathSumMem(int i, int j, int n, List<List<Integer>> triangle, int[][] dp) {
         if (i == n - 1) {
             return triangle.get(n - 1).get(j);
@@ -45,7 +72,9 @@ public class Q8_MinimumPathSumTriangle {
 
         // return findMinPathSum(0, 0, n, triangle);
 
-        return findMinPathSumMem(0, 0, n, triangle, dp);
+        // return findMinPathSumMem(0, 0, n, triangle, dp);
+
+        return findMinPathSumTab(triangle, n);
     }
 
     public static void main(String[] args) {
