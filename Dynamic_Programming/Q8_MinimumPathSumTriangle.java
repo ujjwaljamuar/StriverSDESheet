42c1b1e2d78ef2b844838e6329eca0162bf95805
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-05 20:52:08 +0530

    minimum path sum in a triangular array memoization

--- Dynamic_Programming/Q8_MinimumPathSumTriangle.java
+++ Dynamic_Programming/Q8_MinimumPathSumTriangle.java
@@ -2,7 +2,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Q8_MinimumPathSumTriangle {
-    int findMinPathSumRec(int i, int j, int n, List<List<Integer>> triangle) {
+    int findMinPathSumMem(int i, int j, int n, List<List<Integer>> triangle, int[][] dp) {
+        if (i == n - 1) {
+            return triangle.get(n - 1).get(j);
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        // int down = triangle[i][j] + findMinPathSum(i+1, j);
+        // int diag = triangle[i][j] + findMinPathSum(i+1, j+1);
+
+        int down = triangle.get(i).get(j) + findMinPathSumMem(i + 1, j, n, triangle, dp);
+        int diag = triangle.get(i).get(j) + findMinPathSumMem(i + 1, j + 1, n, triangle, dp);
+
+        return dp[i][j] = Math.min(down, diag);
+    }
+
+    int findMinPathSum(int i, int j, int n, List<List<Integer>> triangle) {
         if (i == n - 1) {
             return triangle.get(n - 1).get(j);
         }
@@ -10,8 +28,8 @@ public class Q8_MinimumPathSumTriangle {
         // int down = triangle[i][j] + findMinPathSum(i+1, j);
         // int diag = triangle[i][j] + findMinPathSum(i+1, j+1);
 
-        int down = triangle.get(i).get(j) + findMinPathSumRec(i + 1, j, n, triangle);
-        int diag = triangle.get(i).get(j) + findMinPathSumRec(i + 1, j + 1, n, triangle);
+        int down = triangle.get(i).get(j) + findMinPathSum(i + 1, j, n, triangle);
+        int diag = triangle.get(i).get(j) + findMinPathSum(i + 1, j + 1, n, triangle);
 
         return Math.min(down, diag);
     }
@@ -19,7 +37,15 @@ public class Q8_MinimumPathSumTriangle {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
 
-        return findMinPathSumRec(0, 0, n, triangle);
+        int[][] dp = new int[n][triangle.get(n - 1).size()];
+
+        for (int[] rows : dp) {
+            Arrays.fill(rows, -1);
+        }
+
+        // return findMinPathSum(0, 0, n, triangle);
+
+        return findMinPathSumMem(0, 0, n, triangle, dp);
     }
 
     public static void main(String[] args) {
