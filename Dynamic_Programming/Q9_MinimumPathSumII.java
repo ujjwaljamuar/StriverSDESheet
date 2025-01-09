94c44362fce2a10f46c0e01cacfbd7ad67b8eb61
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-09 20:26:09 +0530

    minimum path sum in a grid with var entry and end point tabulation
    minimum path sum in a grid with var entry and end point tabulation

--- Dynamic_Programming/Q9_MinimumPathSumII.java
+++ Dynamic_Programming/Q9_MinimumPathSumII.java
@@ -1,6 +1,50 @@
 import java.util.Arrays;
 
 public class Q9_MinimumPathSumII {
+    int findPathSumTab(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int dp[][] = new int[n][m];
+
+        // Initializing the first row - base condition
+        for (int j = 0; j < m; j++) {
+            dp[0][j] = matrix[0][j];
+        }
+
+        // Calculate the maximum path sum for each cell in the matrix
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int up = matrix[i][j] + dp[i - 1][j];
+
+                int leftDiagonal = matrix[i][j];
+                if (j - 1 >= 0) {
+                    leftDiagonal += dp[i - 1][j - 1];
+                } else {
+                    leftDiagonal += (int) Math.pow(10, 9);
+                }
+
+                int rightDiagonal = matrix[i][j];
+                if (j + 1 < m) {
+                    rightDiagonal += dp[i - 1][j + 1];
+                } else {
+                    rightDiagonal += (int) Math.pow(10, 9);
+                }
+
+                // Store the maximum of the three paths in dp
+                dp[i][j] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+            }
+        }
+
+        // Find the maximum value in the last row of dp
+        int mini = Integer.MAX_VALUE;
+        for (int j = 0; j < m; j++) {
+            mini = Math.min(mini, dp[n - 1][j]);
+        }
+
+        return mini;
+    }
+
     int findPathSumMem(int i, int j, int[][] grid, int n, int m, int[][] dp) {
         if (j < 0 || j >= m) {
             return (int) 1e9;
@@ -50,11 +94,13 @@ public class Q9_MinimumPathSumII {
         // mini = Math.min(mini, findPathSumRec(n - 1, j, matrix, n, m));
         // }
 
-        for (int j = 0; j < m; j++) {
-            mini = Math.min(mini, findPathSumMem(n - 1, j, matrix, n, m, dp));
-        }
+        // for (int j = 0; j < m; j++) {
+        // mini = Math.min(mini, findPathSumMem(n - 1, j, matrix, n, m, dp));
+        // }
 
-        return mini;
+        // return mini;
+
+        return findPathSumTab(matrix);
     }
 
     public static void main(String[] args) {
