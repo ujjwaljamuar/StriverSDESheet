6577d413a3e4c74d4e7867683d36f8ac5030ae9b
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-23 19:52:43 +0530

    subset sum equal to target in an array, memoization

--- Dynamic_Programming/Q14_SubsetSumEqualTarget.java
+++ Dynamic_Programming/Q14_SubsetSumEqualTarget.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
+
 public class Q14_SubsetSumEqualTarget {
+    boolean isSubsetSumMem(int[] arr, int index, int target, int[][] dp) {
+        if (target == 0) {
+            return true;
+        }
+
+        if (index == 0) {
+            return arr[0] == target;
+        }
+
+        if (dp[index][target] != -1) {
+            return dp[index][target] == 0 ? false : true;
+        }
+
+        boolean notTake = isSubsetSumRec(arr, index - 1, target);
+        boolean take = false;
+
+        if (target >= arr[index]) {
+            take = isSubsetSumRec(arr, index - 1, target - arr[index]);
+        }
+
+        dp[index][target] = take || notTake ? 0 : 1;
+        return take || notTake;
+    }
 
     boolean isSubsetSumRec(int[] arr, int index, int target) {
         if (target == 0) {
@@ -20,7 +45,15 @@ public class Q14_SubsetSumEqualTarget {
     }
 
     boolean isSubsetSum(int[] arr, int target) {
-        return isSubsetSumRec(arr, arr.length - 1, target);
+        int[][] dp = new int[arr.length][target + 1];
+        for (int[] rows : dp) {
+            Arrays.fill(rows, -1);
+        }
+
+        // return isSubsetSumRec(arr, arr.length - 1, target);
+
+        return isSubsetSumMem(arr, arr.length - 1, target, dp);
+
     }
 
     public static void main(String[] args) {
