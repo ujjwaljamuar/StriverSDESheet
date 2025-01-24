4e6b813ea054bcbd06ce3d7bcad4d910ef7f96ad
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-24 19:49:06 +0530

    partition equal subset sum, Memoization

--- Dynamic_Programming/Q15_PartitionEqualSubset.java
+++ Dynamic_Programming/Q15_PartitionEqualSubset.java
@@ -1,4 +1,30 @@
+import java.util.Arrays;
+
 public class Q15_PartitionEqualSubset {
+    boolean isSubsetSumMem(int[] arr, int index, int target, int[][] dp) {
+        if (target == 0) {
+            return true;
+        }
+
+        if (index == 0) {
+            return arr[0] == target;
+        }
+
+        if (dp[index][target] != -1) {
+            return dp[index][target] == 0 ? false : true;
+        }
+
+        boolean notTake = isSubsetSumMem(arr, index - 1, target, dp);
+        boolean take = false;
+
+        if (target >= arr[index]) {
+            take = isSubsetSumMem(arr, index - 1, target - arr[index], dp);
+        }
+
+        dp[index][target] = take || notTake ? 0 : 1;
+        return take || notTake;
+    }
+
     boolean isSubsetSumRec(int[] arr, int index, int target) {
         if (target == 0) {
             return true;
@@ -29,7 +55,14 @@ public class Q15_PartitionEqualSubset {
         } else {
             int k = totSum / 2;
 
-            return isSubsetSumRec(arr, arr.length - 1, k);
+            int[][] dp = new int[arr.length][k + 1];
+            for (int[] rows : dp) {
+                Arrays.fill(rows, -1);
+            }
+
+            // return isSubsetSumRec(arr, arr.length - 1, k);
+
+            return isSubsetSumMem(arr, arr.length - 1, k, dp);
         }
     }
 
