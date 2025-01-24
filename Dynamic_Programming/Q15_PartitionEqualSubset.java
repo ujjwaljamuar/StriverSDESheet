1e78031dd11d3b87a681b3efdb4846cf489da9bd
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-24 19:52:52 +0530

    partition equal subset sum, Optimized

--- Dynamic_Programming/Q15_PartitionEqualSubset.java
+++ Dynamic_Programming/Q15_PartitionEqualSubset.java
@@ -1,6 +1,42 @@
 import java.util.Arrays;
 
 public class Q15_PartitionEqualSubset {
+    boolean isSubsetSumOpt(int n, int k, int[] arr) {
+        boolean prev[] = new boolean[k + 1];
+
+        // Initialize the first row of the DP table
+        prev[0] = true;
+
+        // Initialize the first column of the DP table
+        if (arr[0] <= k) {
+            prev[arr[0]] = true;
+        }
+
+        // Fill in the DP table using bottom-up dynamic programming
+        for (int ind = 1; ind < n; ind++) {
+            boolean cur[] = new boolean[k + 1];
+            cur[0] = true;
+            for (int target = 1; target <= k; target++) {
+                // Calculate if the current element is not taken
+                boolean notTaken = prev[target];
+
+                // Calculate if the current element is taken
+                boolean taken = false;
+                if (arr[ind] <= target) {
+                    taken = prev[target - arr[ind]];
+                }
+
+                // Update the DP table for the current element and target sum
+                cur[target] = notTaken || taken;
+            }
+            // Update the previous row with the current row for the next iteration
+            prev = cur;
+        }
+
+        // The result is stored in the last cell of the DP table
+        return prev[k];
+    }
+
     boolean isSubsetSumTab(int n, int k, int[] arr) {
         // Create a boolean DP table with dimensions [n][k+1]
         boolean dp[][] = new boolean[n][k + 1];
@@ -100,7 +136,9 @@ public class Q15_PartitionEqualSubset {
 
             // return isSubsetSumMem(arr, arr.length - 1, k, dp);
 
-            return isSubsetSumTab(arr.length, k, arr);
+            // return isSubsetSumTab(arr.length, k, arr);
+
+            return isSubsetSumOpt(arr.length, k, arr);
         }
     }
 
