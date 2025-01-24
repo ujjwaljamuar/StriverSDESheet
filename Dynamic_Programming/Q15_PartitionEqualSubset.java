3be6eecfb5df0fac1ec593ddbdf455ffb6a7e228
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-24 19:51:25 +0530

    partition equal subset sum, Tabulation

--- Dynamic_Programming/Q15_PartitionEqualSubset.java
+++ Dynamic_Programming/Q15_PartitionEqualSubset.java
@@ -1,6 +1,42 @@
 import java.util.Arrays;
 
 public class Q15_PartitionEqualSubset {
+    boolean isSubsetSumTab(int n, int k, int[] arr) {
+        // Create a boolean DP table with dimensions [n][k+1]
+        boolean dp[][] = new boolean[n][k + 1];
+
+        // Initialize the first row of the DP table
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        // Initialize the first column of the DP table
+        if (arr[0] <= k) {
+            dp[0][arr[0]] = true;
+        }
+
+        // Fill in the DP table using bottom-up approach
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+                // Calculate if the current target can be achieved without taking the current
+                // element
+                boolean notTaken = dp[ind - 1][target];
+
+                // Calculate if the current target can be achieved by taking the current element
+                boolean taken = false;
+                if (arr[ind] <= target) {
+                    taken = dp[ind - 1][target - arr[ind]];
+                }
+
+                // Store the result in the DP table
+                dp[ind][target] = notTaken || taken;
+            }
+        }
+
+        // The final result is stored in the bottom-right cell of the DP table
+        return dp[n - 1][k];
+    }
+
     boolean isSubsetSumMem(int[] arr, int index, int target, int[][] dp) {
         if (target == 0) {
             return true;
@@ -62,7 +98,9 @@ public class Q15_PartitionEqualSubset {
 
             // return isSubsetSumRec(arr, arr.length - 1, k);
 
-            return isSubsetSumMem(arr, arr.length - 1, k, dp);
+            // return isSubsetSumMem(arr, arr.length - 1, k, dp);
+
+            return isSubsetSumTab(arr.length, k, arr);
         }
     }
 
