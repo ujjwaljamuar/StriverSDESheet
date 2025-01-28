ad3a45f15941aee815aa28c03c7ee08a4e86f399
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-28 19:14:29 +0530

    length of longest common subseq between two strings, Optimization

--- Dynamic_Programming/Q25_LongestCommonSubseq.java
+++ Dynamic_Programming/Q25_LongestCommonSubseq.java
@@ -1,6 +1,33 @@
 import java.util.Arrays;
 
 public class Q25_LongestCommonSubseq {
+    int lcsUtilOpt(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        // Create arrays to store the LCS lengths
+        int prev[] = new int[m + 1];
+        int cur[] = new int[m + 1];
+
+        // Iterate through the strings and calculate LCS lengths
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                // If the characters at the current indices are the same, increment the LCS
+                // length
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1))
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                // If the characters are different, choose the maximum LCS length by either
+                // excluding a character in s1 or excluding a character in s2
+                else
+                    cur[ind2] = Math.max(prev[ind2], cur[ind2 - 1]);
+            }
+            // Update the 'prev' array to the values of 'cur' for the next iteration
+            prev = (int[]) (cur.clone());
+        }
+
+        return prev[m]; // Return the length of the Longest Common Subsequence (LCS)
+    }
+
     int lcsUtilTab(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
@@ -95,7 +122,9 @@ public class Q25_LongestCommonSubseq {
 
         // return lcsUtilMem(s1, s2, n - 1, m - 1, dp);
 
-        return lcsUtilTab(s1, s2);
+        // return lcsUtilTab(s1, s2);
+
+        return lcsUtilOpt(s1, s2);
     }
 
     public static void main(String args[]) {
