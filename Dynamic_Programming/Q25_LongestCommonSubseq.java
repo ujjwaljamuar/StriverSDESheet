fcf1bc2a9335ecb160117c977dfe37035b452345
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-28 19:11:26 +0530

    length of longest common subseq between two strings, Memoization

--- Dynamic_Programming/Q25_LongestCommonSubseq.java
+++ Dynamic_Programming/Q25_LongestCommonSubseq.java
@@ -1,6 +1,29 @@
 import java.util.Arrays;
 
 public class Q25_LongestCommonSubseq {
+    int lcsUtilMem(String s1, String s2, int ind1, int ind2, int[][] dp) {
+        // Base case: If either of the strings reaches the end, return 0
+        if (ind1 < 0 || ind2 < 0) {
+            return 0;
+        }
+
+        if (dp[ind1][ind2] != -1) {
+            return dp[ind1][ind2];
+        }
+
+        // If the characters at the current indices are the same, increment the LCS
+        // length
+        if (s1.charAt(ind1) == s2.charAt(ind2)) {
+            return dp[ind1][ind2] = 1 + lcsUtilRec(s1, s2, ind1 - 1, ind2 - 1);
+        }
+
+        // If the characters are different, choose the maximum LCS length by either
+        // skipping a character in s1 or skipping a character in s2
+        else {
+            return dp[ind1][ind2] = Math.max(lcsUtilRec(s1, s2, ind1 - 1, ind2), lcsUtilRec(s1, s2, ind1, ind2 - 1));
+        }
+    }
+
     int lcsUtilRec(String s1, String s2, int ind1, int ind2) {
         // Base case: If either of the strings reaches the end, return 0
         if (ind1 < 0 || ind2 < 0) {
@@ -25,7 +48,14 @@ public class Q25_LongestCommonSubseq {
         int m = s2.length();
 
         // Call the recursive function to find the LCS length
-        return lcsUtilRec(s1, s2, n - 1, m - 1);
+        // return lcsUtilRec(s1, s2, n - 1, m - 1);
+
+        int dp[][] = new int[n][m];
+
+        for (int rows[] : dp)
+            Arrays.fill(rows, -1);
+
+        return lcsUtilMem(s1, s2, n - 1, m - 1, dp);
     }
 
     public static void main(String args[]) {
