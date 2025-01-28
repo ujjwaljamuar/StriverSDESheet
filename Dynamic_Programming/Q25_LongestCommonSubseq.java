13c52582927b812f71d29f242676fbabc654e9f0
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-01-28 19:12:09 +0530

    length of longest common subseq between two strings, Tabulation

--- Dynamic_Programming/Q25_LongestCommonSubseq.java
+++ Dynamic_Programming/Q25_LongestCommonSubseq.java
@@ -1,6 +1,44 @@
 import java.util.Arrays;
 
 public class Q25_LongestCommonSubseq {
+    int lcsUtilTab(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        // Create a 2D array to store results of subproblems
+        int dp[][] = new int[n + 1][m + 1];
+
+        // Initialize the dp array with -1 to indicate that subproblems are not solved
+        // yet
+        for (int rows[] : dp)
+            Arrays.fill(rows, -1);
+
+        // Initialize the first row and first column with zeros since LCS with an empty
+        // string is zero
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
+
+        // Fill the dp array using dynamic programming
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                // If the characters at the current indices are the same, increment the LCS
+                // length
+                if (s1.charAt(ind1 - 1) == s2.charAt(ind2 - 1))
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                // If the characters are different, choose the maximum LCS length by either
+                // excluding a character in s1 or excluding a character in s2
+                else
+                    dp[ind1][ind2] = Math.max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+
+        return dp[n][m]; // Return the length of the Longest Common Subsequence (LCS)
+    }
+
     int lcsUtilMem(String s1, String s2, int ind1, int ind2, int[][] dp) {
         // Base case: If either of the strings reaches the end, return 0
         if (ind1 < 0 || ind2 < 0) {
@@ -55,7 +93,9 @@ public class Q25_LongestCommonSubseq {
         for (int rows[] : dp)
             Arrays.fill(rows, -1);
 
-        return lcsUtilMem(s1, s2, n - 1, m - 1, dp);
+        // return lcsUtilMem(s1, s2, n - 1, m - 1, dp);
+
+        return lcsUtilTab(s1, s2);
     }
 
     public static void main(String args[]) {
