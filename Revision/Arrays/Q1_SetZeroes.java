203f664fa2b10dfa98eb7ca1255d8ba0a18cbb73
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-02-25 19:44:56 +0530

    set zeroes in a 2d array, optimal approach

--- Revision/Arrays/Q1_SetZeroes.java
+++ Revision/Arrays/Q1_SetZeroes.java
@@ -1,6 +1,57 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Q1_SetZeroes {
+    //  TC: O(2*(N*M))
+    ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+        // int[] row = new int[n]; --> matrix[..][0]
+        // int[] col = new int[m]; --> matrix[0][..]
+
+        int col0 = 1;
+        // step 1: Traverse the matrix and
+        // mark 1st row & col accordingly:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    // mark i-th row:
+                    matrix.get(i).set(0, 0);
+
+                    // mark j-th column:
+                    if (j != 0)
+                        matrix.get(0).set(j, 0);
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix.get(i).get(j) != 0) {
+                    // check for col & row:
+                    if (matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0) {
+                        matrix.get(i).set(j, 0);
+                    }
+                }
+            }
+        }
+
+        // step 3: Finally mark the 1st col & then 1st row:
+        if (matrix.get(0).get(0) == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix.get(0).set(j, 0);
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix.get(i).set(0, 0);
+            }
+        }
+
+        return matrix;
+    }
+
     // better approach
     // TC ~ O(n)^2
     void setZeroes(int[][] arr) {
