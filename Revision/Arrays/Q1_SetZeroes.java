d541614beae00b8b002e9eb42ec8e6d6540dbabe
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-02-25 18:57:49 +0530

    set zeroes in a 2d array, better approach

--- Revision/Arrays/Q1_SetZeroes.java
+++ Revision/Arrays/Q1_SetZeroes.java
@@ -1,7 +1,32 @@
 import java.util.Arrays;
 
 public class Q1_SetZeroes {
-    // brute force 
+    // better approach
+    // TC ~ O(n)^2
+    void setZeroes(int[][] arr) {
+        int[] row = new int[arr[0].length];
+        int[] col = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j] == 0) {
+                    row[j] = 1;
+                    col[i] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if ((row[i] == 1) || (col[j] == 1)) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+    // brute force
     // TC ~ O(n)^3
     void markRow(int[][] arr, int i) {
         for (int j = 0; j < arr[0].length; j++) {
@@ -43,7 +68,8 @@ public class Q1_SetZeroes {
 
         int[][] arr = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
 
-        sz.findZeroes(arr);
+        // sz.findZeroes(arr);
+        sz.setZeroes(arr);
 
         System.out.println(Arrays.deepToString(arr));
     }
