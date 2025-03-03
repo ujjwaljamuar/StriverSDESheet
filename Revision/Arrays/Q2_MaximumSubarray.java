35865e319fb37f42617875ff0921578ed4e126de
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-04 01:21:34 +0530

    maximum sum subarray, optimal using two loops

--- Revision/Arrays/Q2_MaximumSubarray.java
+++ Revision/Arrays/Q2_MaximumSubarray.java
@@ -1,4 +1,23 @@
 public class Q2_MaximumSubarray {
+    int maxSubarraySumBetter(int[] arr) {
+        int maxi = Integer.MIN_VALUE; // maximum sum
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                // current subarray = arr[i.....j]
+
+                // add the current element arr[j]
+                // to the sum i.e. sum of arr[i...j-1]
+                sum += arr[j];
+
+                maxi = Math.max(maxi, sum); // getting the maximum
+            }
+        }
+
+        return maxi;
+    }
+
     int maxSumSubArrBrute(int[] arr) {
         int max = Integer.MIN_VALUE;
 
@@ -7,7 +26,7 @@ public class Q2_MaximumSubarray {
                 // subarray = arr[i.....j]
                 int sum = 0;
 
-                //add all the elements of subarray:
+                // add all the elements of subarray:
                 for (int k = i; k <= j; k++) {
                     sum += arr[k];
                 }
@@ -23,6 +42,7 @@ public class Q2_MaximumSubarray {
 
         int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 
-        System.out.println(ms.maxSumSubArrBrute(arr));
+        // System.out.println(ms.maxSumSubArrBrute(arr));
+        System.out.println(ms.maxSubarraySumBetter(arr));
     }
 }
