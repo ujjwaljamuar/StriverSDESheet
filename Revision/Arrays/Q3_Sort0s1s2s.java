aa3b92a487468dca79064029772fdb6fb4d90c57
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-04 23:56:23 +0530

    sort 0s1s2s, brute

---
+++ Revision/Arrays/Q3_Sort0s1s2s.java
@@ -0,0 +1,12 @@
+public class Q3_Sort0s1s2s {
+
+    // brute
+    // use any sort algo, probably merge sort
+    // TC - o(nlogn)
+    // SC - o(n)
+    public static void main(String[] args) {
+        Q3_Sort0s1s2s ar = new Q3_Sort0s1s2s();
+
+        int[] arr = { 2, 0, 2, 1, 1, 0 };
+    }
+}
