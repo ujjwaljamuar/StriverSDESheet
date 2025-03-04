3ccc093251225e699f09aea194f9e825d860d554
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-05 00:04:40 +0530

    sort 0s1s2s, better using counting

--- Revision/Arrays/Q3_Sort0s1s2s.java
+++ Revision/Arrays/Q3_Sort0s1s2s.java
@@ -1,4 +1,31 @@
+import java.util.Arrays;
+
 public class Q3_Sort0s1s2s {
+
+    // better, using counting
+    // TC - O(2N), SC - O(1)
+    void sortUsingCounting(int[] arr) {
+        int count0 = 0, count1 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                count0++;
+            } else if (arr[i] == 1) {
+                count1++;
+            }
+        }
+
+        for (int i = 0; i < count0; i++) {
+            arr[i] = 0;
+        }
+        for (int i = count0; i < count0 + count1; i++) {
+            arr[i] = 1;
+        }
+        for (int i = count0 + count1; i < arr.length; i++) {
+            arr[i] = 2;
+        }
+
+    }
 
     // brute
     // use any sort algo, probably merge sort
@@ -8,5 +35,7 @@ public class Q3_Sort0s1s2s {
         Q3_Sort0s1s2s ar = new Q3_Sort0s1s2s();
 
         int[] arr = { 2, 0, 2, 1, 1, 0 };
+        ar.sortUsingCounting(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
