8304a5ea43659720490c8f2ba6b0c35d577f3a92
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-31 21:12:04 +0530

    merge two sorted arrays - optimal 1, no extra space

--- Revision/Arrays/Q7_Merge_Two_Sorted_Arrays.java
+++ Revision/Arrays/Q7_Merge_Two_Sorted_Arrays.java
@@ -1,6 +1,26 @@
 import java.util.Arrays;
 
 public class Q7_Merge_Two_Sorted_Arrays {
+    void mergeOptimal1(long[] arr1, long[] arr2, int n, int m) {
+        int left = n - 1;
+        int right = 0;
+        while (left >= 0 && right < m) {
+            if (arr1[left] > arr2[right]) {
+                // swap
+                long temp = arr1[left];
+                arr1[left] = arr2[right];
+                arr2[right] = temp;
+                left--;
+                right++;
+            } else {
+                break;
+            }
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
     void mergeBrute(long[] arr1, long[] arr2, int n, int m) {
         int left = 0;
         int right = 0;
@@ -50,7 +70,8 @@ public class Q7_Merge_Two_Sorted_Arrays {
         long[] arr2 = { 2, 3, 9 };
         int n = 4, m = 3;
 
-        ar.mergeBrute(arr1, arr2, n, m);
+        // ar.mergeBrute(arr1, arr2, n, m);
+        ar.mergeOptimal1(arr1, arr2, n, m);
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr2));
     }
