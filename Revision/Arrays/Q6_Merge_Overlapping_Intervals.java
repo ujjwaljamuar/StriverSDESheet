7a6968ff21d365278a43d1e621b66ed54345cf8e
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-31 20:59:32 +0530

    merge overlapping intervals - optimal

--- Revision/Arrays/Q6_Merge_Overlapping_Intervals.java
+++ Revision/Arrays/Q6_Merge_Overlapping_Intervals.java
@@ -4,6 +4,26 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Q6_Merge_Overlapping_Intervals {
+    List<int[]> mergeOptimal(int[][] intervals){
+        int n = intervals.length;
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        List<int[]> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            if(ans.isEmpty() || intervals[i][0] > ans.get(ans.size() - 1)[1]){
+                ans.add(intervals[i]);
+            }
+            else{
+                ans.get(ans.size() - 1)[1] = Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
+            }
+        } 
+        return ans;
+    }
 
     List<int[]> mergeBrute(int[][] intervals) {
         int n = intervals.length;
@@ -51,6 +71,7 @@ public class Q6_Merge_Overlapping_Intervals {
                 { 8, 10 },
         };
 
+        // System.out.println(Arrays.deepToString((ar.mergeBrute(intervals).toArray())));
         System.out.println(Arrays.deepToString((ar.mergeBrute(intervals).toArray())));
     }
 }
