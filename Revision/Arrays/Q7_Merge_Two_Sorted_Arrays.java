4261693f46ff869bba824ef02437053ca0ff65ad
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-03-31 21:24:06 +0530

    merge two sorted arrays - optimal 2, no extra space

--- Revision/Arrays/Q7_Merge_Two_Sorted_Arrays.java
+++ Revision/Arrays/Q7_Merge_Two_Sorted_Arrays.java
@@ -1,6 +1,44 @@
 import java.util.Arrays;
 
 public class Q7_Merge_Two_Sorted_Arrays {
+    void mergeOptimal2(long[] arr1, long[] arr2, int n, int m) {
+
+        // len of the imaginary single array:
+        int len = n + m;
+
+        // Initial gap:
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            // Place 2 pointers:
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                // case 1: left in arr1[]
+                // and right in arr2[]:
+                if (left < n && right >= n) {
+                    swapIfGreater(arr1, arr2, left, right - n);
+                }
+                // case 2: both pointers in arr2[]:
+                else if (left >= n) {
+                    swapIfGreater(arr2, arr2, left - n, right - n);
+                }
+                // case 3: both pointers in arr1[]:
+                else {
+                    swapIfGreater(arr1, arr1, left, right);
+                }
+                left++;
+                right++;
+            }
+            // break if iteration gap=1 is completed:
+            if (gap == 1)
+                break;
+
+            // Otherwise, calculate new gap:
+            gap = (gap / 2) + (gap % 2);
+        }
+    }
+
     void mergeOptimal1(long[] arr1, long[] arr2, int n, int m) {
         int left = n - 1;
         int right = 0;
@@ -63,6 +101,14 @@ public class Q7_Merge_Two_Sorted_Arrays {
         }
     }
 
+    void swapIfGreater(long[] arr1, long[] arr2, int ind1, int ind2) {
+        if (arr1[ind1] > arr2[ind2]) {
+            long temp = arr1[ind1];
+            arr1[ind1] = arr2[ind2];
+            arr2[ind2] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         Q7_Merge_Two_Sorted_Arrays ar = new Q7_Merge_Two_Sorted_Arrays();
 
@@ -71,7 +117,8 @@ public class Q7_Merge_Two_Sorted_Arrays {
         int n = 4, m = 3;
 
         // ar.mergeBrute(arr1, arr2, n, m);
-        ar.mergeOptimal1(arr1, arr2, n, m);
+        // ar.mergeOptimal1(arr1, arr2, n, m);
+        ar.mergeOptimal2(arr1, arr2, n, m);
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr2));
     }
