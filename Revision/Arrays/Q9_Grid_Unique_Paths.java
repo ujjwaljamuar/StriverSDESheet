22e61a05624966a98b3970c3c6fee0ecde36d5a7
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-04-03 19:03:05 +0530

    find unique paths optimal using combinatrix nCr

--- Revision/Arrays/Q9_Grid_Unique_Paths.java
+++ Revision/Arrays/Q9_Grid_Unique_Paths.java
@@ -1,7 +1,18 @@
 public class Q9_Grid_Unique_Paths {
+    int findPathsCombinationSoln(int m, int n) {
+        // we have m+n-2 soln always with m-1 right path or n-1 bottom paths find soln using nCr
+        int N = n + m - 2;
+        int r = m - 1;
+        double res = 1;
+
+        for (int i = 1; i <= r; i++)
+            res = res * (N - r + i) / i;
+        return (int) res;
+    }
+
     // go every possible path and return 1 if found and sum it up
     int findPathsRecursion(int i, int j, int m, int n) {
-        if (i == m-1 && j == n-1) {
+        if (i == m - 1 && j == n - 1) {
             return 1;
         }
         if (i >= m || j >= n) {
@@ -15,6 +26,7 @@ public class Q9_Grid_Unique_Paths {
         Q9_Grid_Unique_Paths bs = new Q9_Grid_Unique_Paths();
 
         int m = 3, n = 7;
-        System.out.println(bs.findPathsRecursion(0, 0, m, n));
+        // System.out.println(bs.findPathsRecursion(0, 0, m, n));
+        System.out.println(bs.findPathsCombinationSoln(m, n));
     }
 }
