6f905e8ea9c4e41bde397e9703d908e1cd539f79
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2025-09-30 17:46:55 +0530

    Climbing Stairs Problem

---
+++ Revision/DP/Q1_ClimbingStairs.java
@@ -0,0 +1,25 @@
+package DP;
+
+public class Q1_ClimbingStairs {
+    int climbStairs(int n) {
+        if(n == 0 || n == 1){
+            return 1;
+        }
+
+        return climbStairs(n-2) + climbStairs(n-1);
+    }
+    int climbStairsMem(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        
+    }
+}
